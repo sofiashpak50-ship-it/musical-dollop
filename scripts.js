c4a081611f39f32607083f0e062b08af1439c707
trosslolllll@gmail.com <trosslolllll@gmail.com>
2026-06-25 10:01:15 +0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.filter-btn').forEach(button => {
+            button.addEventListener('click', () => {
+                document.querySelectorAll('.filter-btn').forEach(btn => btn.classList.remove('active'));
+                button.classList.add('active');
+            });
+        });
